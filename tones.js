7bef939525e70cf7440d6130ac2e401e603d220d
Horea Porutiu <horeaporutiu@gmail.com>
2017-11-28 11:06:49 -0800

    indent

--- tones.js
+++ tones.js
@@ -5,21 +5,21 @@ var toneUsername= "Watson Tone Analyzer Service Username";
 var tonePassword= "Watson Tone Analyzer Service Username";
 var toneUrl = "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21";
 
-    var data = {};
-    data.text = "I can't wait to see you :)";
+var data = {};
+data.text = "I can't wait to see you :)";
 
-    request.post({
-        url: toneUrl,
-        json: data,
-        auth: {
-            user: toneUsername,
-            pass: tonePassword
-        }
-    
-    }, function (err, response, body){
-        console.log(body);
+request.post({
+    url: toneUrl,
+    json: data,
+    auth: {
+        user: toneUsername,
+        pass: tonePassword
+    }
 
-        console.log(body.document_tone.tones[0].tone_name +
-        ", Score: " + body.document_tone.tones[0].score);
-    });
+}, function (err, response, body){
+    console.log(body);
+
+    console.log(body.document_tone.tones[0].tone_name +
+    ", Score: " + body.document_tone.tones[0].score);
+});
 
